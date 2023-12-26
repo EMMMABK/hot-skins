f79321b46508cff745c2f3b2784b654697f1d9f4
EMMMABK <adilbikiev2@gmail.com>
2023-12-26 23:01:01 +0600

    add HomePage

---
+++ js/index.js
@@ -0,0 +1,21 @@
+let modal = document.getElementById("myModal");
+let overlay = document.getElementById("overlay");
+let btn = document.getElementById("connectButton");
+let span = document.getElementById("closeModal");
+
+btn.onclick = function() {
+    modal.classList.add("show");
+    overlay.classList.add("show"); 
+};
+
+span.onclick = function() {
+    modal.classList.remove("show");
+    overlay.classList.remove("show"); 
+};
+
+window.onclick = function(event) {
+    if (event.target == overlay) {
+        modal.classList.remove("show");
+        overlay.classList.remove("show"); 
+    }
+};
