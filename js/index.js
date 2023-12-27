af84584094593c980acfe485546a647c47c40221
EMMMABK <adilbikiev2@gmail.com>
2023-12-27 12:28:25 +0600

    add 12-commit Reg

--- js/index.js
+++ js/index.js
@@ -21,5 +21,25 @@ window.onclick = function(event) {
 };
 
 
+let modal_auth = document.getElementById("myModal_auth");
+let overlay_auth = document.getElementById("overlay_auth");
+let btn_auth = document.getElementById("button__auth");
+let span_auth = document.getElementById("closeModal_auth");
 
+btn_auth.onclick = function() {
+    modal_auth.classList.add("show");
+    overlay_auth.classList.add("show"); 
+};
+
+span_auth.onclick = function() {
+    modal_auth.classList.remove("show");
+    overlay_auth.classList.remove("show"); 
+};
+
+window.onclick = function(event) {
+    if (event.target == overlay_auth) {
+        modal_auth.classList.remove("show");
+        overlay_auth.classList.remove("show"); 
+    }
+};
 
